6c563ff012d37e5dba568a49448ba32e79af0f50
Kiwifan1 <70110252+Kiwifan1@users.noreply.github.com>
2020-08-23 12:04:23 -0700

    Changed

--- EnigmaDevice.java
+++ EnigmaDevice.java
@@ -13,7 +13,7 @@ public class EnigmaDevice{
         */
         PlugBoard plugboard = new PlugBoard();
         Scanner input = new Scanner(System.in);
-        boolean playing = true;
+        boolean  playing = true;
         char[][] alphabet = {{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'},
                              {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'}};
         
