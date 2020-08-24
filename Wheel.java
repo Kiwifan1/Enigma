27193376fa51ec0d94e69dd96c9a75735cebef94
Kiwifan1 <70110252+Kiwifan1@users.noreply.github.com>
2020-08-23 20:56:59 -0700

    FIxed the double array in Wheel.java along with the call in EnigmaDevice

--- Wheel.java
+++ Wheel.java
@@ -6,25 +6,26 @@ public class Wheel{
 
     //makes and returns the Wheel based on the input given in constructor
     public char[][] CreateWheel(int TypeWheel){
-        if(TypeWheel == 1){
+        if(TypeWheel == 0){
             char[][] Wheel = {{'a', 'b', 'c', 'd','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'},
                               {'x','y','z','a','b','c','d','e','f','g','h','o','j','k','l','m','n','o','p','q','r','s','t','u','v','w'}};
             return Wheel;
         }
-        else if(TypeWheel == 2){
+        else if(TypeWheel == 1){
             char[][] Wheel = {{'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j'},
                               {'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f'}};
             
             return Wheel;
         }
-        else if(TypeWheel == 3){
+        else if(TypeWheel == 2){
             char[][] Wheel = {{'p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o'},
                               {'s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'}};
             
             return Wheel;
         }
-        else if(TypeWheel == 4){
-            char[][] Wheel = {{'m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l'}};
+        else if(TypeWheel == 3){
+            char[][] Wheel = {{'m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j','k','l'},
+                              {'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j'}};
             return Wheel;
         }
         else{
